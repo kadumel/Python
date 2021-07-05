c8b308854877ec74f285e4626cc56731ffb89200
Daniel <daniellnogueira@hotmail.com>
2021-07-05 17:44:16 -0300

    Alteracao dos modelos 4

--- core/models.py
+++ core/models.py
@@ -106,7 +106,7 @@ class Produto(models.Model):
         db_table = 'Produto'
 
     def __str__(self):
-        return self.idproduto, self.cdsubgrupo_id , self.nmproduto
+        return self.nmproduto
 
 
 class Subgrupo(models.Model):
